b36f6c0f44236ec859364c56dd6481f25b947289
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-22 20:46:18 -0700

    Add function to show script from speech recognition.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import "./styles.css";
 
-(function(){
+(function() {
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+  let p = document.createElement('p');
+  const words = document.querySelector('.words');
+  words.appendChild(p);
 
-});
+  recognition.addEventListener('result', event => {
+    const transcript = Array.from(event.results)
+      .map(result => result[0])
+      .map(result => result.transcript).join('');
+
+    // Create a new container if you make a pause.
+    if(event.results[0].isFinal) {
+      p = document.createElement('p');
+      words.appendChild(p);
+    }
+    p.textContent = transcript;
+  });
+  
+  recognition.start();
+  // Start recognition after each pause
+  recognition.addEventListener('end', recognition.start);
+}());
