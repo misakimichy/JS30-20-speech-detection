e0286031cddaa054f218174dd8dbbb8979f72ccb
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-22 21:35:37 -0700

    Add transcript to p element.

--- src/main.js
+++ src/main.js
@@ -1,27 +1,28 @@
 import "./styles.css";
 
-(function() {
+(function(){
   window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = new SpeechRecognition();
+  const recognition = new window.SpeechRecognition();
   recognition.interimResults = true;
+  
   let p = document.createElement('p');
   const words = document.querySelector('.words');
   words.appendChild(p);
 
-  recognition.addEventListener('result', event => {
-    const transcript = Array.from(event.results)
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
       .map(result => result[0])
       .map(result => result.transcript).join('');
 
     // Create a new container if you make a pause.
-    if(event.results[0].isFinal) {
+    if(e.results[0].isFinal) {
       p = document.createElement('p');
       words.appendChild(p);
     }
     p.textContent = transcript;
   });
   
-  recognition.start();
   // Start recognition after each pause
   recognition.addEventListener('end', recognition.start);
+  recognition.start();
 }());
